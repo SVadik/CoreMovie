936a34c9b6ac472075213ed474d11408097e24ad
Sagitov.V <v.sagitov@bk.ru>
2020-02-06 01:28:52 +0500

    Добавьте файлы проекта.

---
+++ Data/CoreMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CoreMovie.Models;
+
+namespace CoreMovie.Data
+{
+    public class CoreMovieContext : DbContext
+    {
+        public CoreMovieContext (DbContextOptions<CoreMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CoreMovie.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CoreMovie.Data;
+using CoreMovie.Models;
+
+namespace CoreMovie
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly CoreMovie.Data.CoreMovieContext _context;
+
+        public DetailsModel(CoreMovie.Data.CoreMovieContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
